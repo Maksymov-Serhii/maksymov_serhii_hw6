03ff1094278e9f42f2dbfc00f6fa87e157b3cf46
Maksymov-Serhii <maksymov.serhii.10.01@gmail.com>
2023-04-11 02:41:41 +0300

    refactored UserLogin component

--- src/components/UserLogin/UserLogin.js
+++ src/components/UserLogin/UserLogin.js
@@ -2,15 +2,19 @@ import React from "react";
 import "./UserLogin.scss"
 
 class UserLogin extends React.Component {
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log("submit success");
+  }
   render() {
     return (
       <div className='user-login-container'>
         <div className="container">
           <h3>User Login</h3>
           <div className="form">
-            <form action="#" className="login-form">
+            <form onSubmit={(event) => this.handleSubmit(event)} className="login-form">
               <input type="text" name="userName" placeholder="Username"></input>
-              <input type="text" name="password" placeholder="Password"></input>
+              <input type="password" name="userPassword" placeholder="Password"></input>
               <button type="submit" className="btn">Login</button>
               <p className="message">Already registered?<a href="#"> Sign in</a></p>
           </form>
