3dffa8d52ff54bf5c09984c353d3a1358761393a
Maksymov-Serhii <maksymov.serhii.10.01@gmail.com>
2023-04-11 01:26:07 +0300

    added validation to input elements in ContactUs component

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -8,6 +8,7 @@ class ContactUs extends React.Component {
       userName: '',
       userEmail: '',
       userMessage: '',
+      errors: {},
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -16,28 +17,47 @@ class ContactUs extends React.Component {
   
   handleChange(event) {
     const { name, value } = event.target;
-    this.setState({ [name]: value })
+    const errors = { ...this.state.errors };
+    delete errors[name];
+    this.setState({ [name]: value, errors });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    this.setState(this.defaultState);
-    console.log("submit success");
+    const { userName, userEmail, userMessage } = this.state;
+    const errors = {};
+    if (userName === '') {
+      errors.userName = true;
+    }
+    if (userEmail === '') {
+      errors.userEmail = true;
+    }
+    if (userMessage === '') {
+      errors.userMessage = true;
+    }
+
+    if (Object.keys(errors).length === 0) {
+      this.setState(this.defaultState);
+      console.log("submit success");
+    } else {
+      this.setState({ errors });
+    }
   }
 
   render() {
+    const { errors } = this.state;
     return (
       <div className={styles.container}>
         <h3 className={styles['contact-header']}>Contact Us</h3>
         <form onSubmit={this.handleSubmit}>
           <div className={styles['form-block']}>
-            <input type="text" className={styles['input-field']} placeholder="Full Name" name="userName" value={this.state.userName} onChange={this.handleChange} required></input>
+            <input type="text" className={`${styles['input-field']} ${errors.userName ? styles['alert-border'] : ''}`} placeholder="Full Name" name="userName" value={this.state.userName} onChange={this.handleChange}></input>
           </div>
           <div className={styles['form-block']}>
-            <input type="email" className={styles['input-field']} placeholder="Email" name="userEmail" value={this.state.userEmail} onChange={this.handleChange}></input>
+            <input type="email" className={`${styles['input-field']} ${errors.userEmail ? styles['alert-border'] : ''}`} placeholder="Email" name="userEmail" value={this.state.userEmail} onChange={this.handleChange}></input>
           </div>
           <div className={styles['form-block']}>
-            <textarea className={styles['textarea-field']} placeholder="Your Message"  name="userMessage" value={this.state.userMessage} onChange={this.handleChange} required />
+            <textarea className={`${styles['textarea-field']} ${errors.userMessage ? styles['alert-border'] : ''}`} placeholder="Your Message"  name="userMessage" value={this.state.userMessage} onChange={this.handleChange} />
           </div>
           <div className={styles['submit-block']}>
             <input type="submit" value="SEND EMAIL" className={styles['submit-button']} />
